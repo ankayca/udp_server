17cefb37d87a2d72cfba9a5f9ae0d0235698d269
Ahmet Adin <57046103+ankayca@users.noreply.github.com>
2020-10-29 00:40:41 +0300

    Add files via upload
    mööm

--- main/udp_server.c
+++ main/udp_server.c
@@ -1,11 +1,3 @@
-/* BSD Socket API Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <string.h>
 #include <sys/param.h>
 #include "esp_wifi.h"
@@ -19,6 +11,8 @@
 #include "nvs.h"
 #include "nvs_flash.h"
 
+#include "driver/uart.h"
+#include "freertos/queue.h"
 #include "lwip/err.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
@@ -30,95 +24,113 @@
 #define EXAMPLE_MAX_STA_CONN       4
 static const char *TAG = "example";
 
+
+
+#define EX_UART_NUM UART_NUM_0
+
+#define BUF_SIZE (1024)
+#define RD_BUF_SIZE (BUF_SIZE)
+static QueueHandle_t uart0_queue;
+void uart_init();
+
 static void udp_server_task(void *pvParameters)
 {
+	char tx_buffer[128];
     char rx_buffer[128];
     char addr_str[128];
     int addr_family;
     int ip_protocol;
-
+    uint8_t len2=0;
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+    uart_init();
     while (1) {
 
-#ifdef CONFIG_EXAMPLE_IPV4
-        struct sockaddr_in destAddr;
-        destAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-        destAddr.sin_family = AF_INET;
-        destAddr.sin_port = htons(PORT);
-        addr_family = AF_INET;
-        ip_protocol = IPPROTO_IP;
-        inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
-#else // IPV6
-        struct sockaddr_in6 destAddr;
-        bzero(&destAddr.sin6_addr.un, sizeof(destAddr.sin6_addr.un));
-        destAddr.sin6_family = AF_INET6;
-        destAddr.sin6_port = htons(PORT);
-        addr_family = AF_INET6;
-        ip_protocol = IPPROTO_IPV6;
-        inet6_ntoa_r(destAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
-#endif
+            struct sockaddr_in destAddr;
+            destAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+            destAddr.sin_family = AF_INET;
+            destAddr.sin_port = htons(PORT);
+            addr_family = AF_INET;
+            ip_protocol = IPPROTO_IP;
+            inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
 
-        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
-        if (sock < 0) {
-            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-            break;
-        }
-        ESP_LOGI(TAG, "Socket created");
 
-        int err = bind(sock, (struct sockaddr *)&destAddr, sizeof(destAddr));
-        if (err < 0) {
-            ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
-        }
-        ESP_LOGI(TAG, "Socket binded");
-
-        while (1) {
-
-            ESP_LOGI(TAG, "Waiting for data");
-#ifdef CONFIG_EXAMPLE_IPV6
-            struct sockaddr_in6 sourceAddr; // Large enough for both IPv4 or IPv6
-#else
-            struct sockaddr_in sourceAddr;
-#endif
-            socklen_t socklen = sizeof(sourceAddr);
-            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&sourceAddr, &socklen);
-
-            // Error occured during receiving
-            if (len < 0) {
-                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+            int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+            if (sock < 0) {
+                ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
                 break;
             }
-            // Data received
-            else {
-                // Get the sender's ip address as string
-#ifdef CONFIG_EXAMPLE_IPV6
-                if (sourceAddr.sin6_family == PF_INET) {
-                    inet_ntoa_r(((struct sockaddr_in *)&sourceAddr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
-                } else if (sourceAddr.sin6_family == PF_INET6) {
-                    inet6_ntoa_r(sourceAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
-                }
-#else
-                inet_ntoa_r(((struct sockaddr_in *)&sourceAddr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
-#endif
+            ESP_LOGI(TAG, "Socket created");
 
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
-                ESP_LOGI(TAG, "%s", rx_buffer);
+            int err = bind(sock, (struct sockaddr *)&destAddr, sizeof(destAddr));
+            if (err < 0) {
+                ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+            }
+            ESP_LOGI(TAG, "Socket binded");
 
-                int err = sendto(sock, rx_buffer, len, 0, (struct sockaddr *)&sourceAddr, sizeof(sourceAddr));
-                if (err < 0) {
-                    ESP_LOGE(TAG, "Error occured during sending: errno %d", errno);
+            while (1) {
+
+                ESP_LOGI(TAG, "Waiting for data");
+
+                struct sockaddr_in sourceAddr;
+
+                socklen_t socklen = sizeof(sourceAddr);
+                int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&sourceAddr, &socklen);
+
+                // Error occured during receiving
+                if (len < 0) {
+                    ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
                     break;
                 }
+                // Data received
+                else {
+                    // Get the sender's ip address as string
+
+                    inet_ntoa_r(((struct sockaddr_in *)&sourceAddr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
+
+
+                    rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
+                    ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+                    ESP_LOGI(TAG, "%s", rx_buffer);
+
+                    uart_flush(UART_NUM_0);
+					while(len2<=5){
+
+					len2 = uart_read_bytes(UART_NUM_0, data, BUF_SIZE, 20 / portTICK_RATE_MS);
+					ESP_LOGE(TAG, "girdim:  %d  girdim",len2);
+						}
+
+
+					sendto(sock, data, len2, 0, (struct sockaddr *)&sourceAddr, sizeof(sourceAddr));
+					uart_flush(UART_NUM_0);
+					len2=0;
+
+
+                    int err = sendto(sock, rx_buffer, len, 0, (struct sockaddr *)&sourceAddr, sizeof(sourceAddr));
+                    if (err < 0) {
+                        ESP_LOGE(TAG, "Error occured during sending: errno %d", errno);
+                        break;
+                    }
+                }
+            }
+
+            if (sock != -1) {
+                ESP_LOGE(TAG, "Shutting down socket and restarting...");
+                shutdown(sock, 0);
+                close(sock);
             }
         }
-
-        if (sock != -1) {
-            ESP_LOGE(TAG, "Shutting down socket and restarting...");
-            shutdown(sock, 0);
-            close(sock);
-        }
+        vTaskDelete(NULL);
     }
-    vTaskDelete(NULL);
-}
+//                while(len2<=5){
+//                len2 = uart_read_bytes(UART_NUM_0, data, BUF_SIZE, 20 / portTICK_RATE_MS);
+//                ESP_LOGE(TAG, "girdim:  %d  girdim",len2);
+//                }
+
+               // ESP_LOGE(TAG, "DATA: %u", data);
+                //sendto(sock, tx_buffer, strlen(tx_buffer), 0, (struct sockaddr *)&sourceAddr, sizeof(sourceAddr));
+               // sendto(sock, "hello",5, 0, (struct sockaddr *)&sourceAddr, sizeof(sourceAddr));
+                //int err = sendto(sock, data,len2, 0, (struct sockaddr *)&sourceAddr, sizeof(sourceAddr));
+
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                     int32_t event_id, void* event_data)
 {
@@ -126,10 +138,13 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
                  MAC2STR(event->mac), event->aid);
+        xTaskCreate(udp_server_task, "udp_server", 4096, NULL, 5, NULL);
+
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
         wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
                  MAC2STR(event->mac), event->aid);
+
     }
 }
 
@@ -163,17 +178,24 @@ void wifi_init_softap()
     ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s",
              EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
 }
+void uart_init(){
+	uart_config_t uart_config = {
+	        .baud_rate = 9600,
+	        .data_bits = UART_DATA_8_BITS,
+	        .parity = UART_PARITY_DISABLE,
+	        .stop_bits = UART_STOP_BITS_1,
+	        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+	    };
+	    uart_param_config(EX_UART_NUM, &uart_config);
+	    uart_driver_install(EX_UART_NUM, BUF_SIZE * 2, BUF_SIZE * 2, 100, &uart0_queue, 0);
+
+}
 void app_main()
 {
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
-   // ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    //ESP_ERROR_CHECK(nvs_flash_init());
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
     wifi_init_softap();
-
-
-    xTaskCreate(udp_server_task, "udp_server", 4096, NULL, 5, NULL);
 }
